931f5fa023fb5d301260fe629319ff6c42597ef4
Michael Psenka <michael.psenka@gmail.com>
2022-04-18 23:32:00 -0700

    curvature generator 1.0

--- test_files/cc_test.py
+++ test_files/cc_test.py
@@ -5,151 +5,162 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-
-# dimension of data
-d = 2
-# number of samples
-n = 25
-# variance of noise
-eps2 = 0
-# determine if we should randomly rotate
-rotate=False
+import sys
+# setting path
+sys.path.append('../')
+from tools.manifold_generator import Manifold
 
 
-# 2D manifold in 4D space
+hi = Manifold(D=2, d=1, curvature=0.5, n_basis=10)
+X = hi.generateSample(N=500, uniform=True)
+plt.plot(X[0,:], X[1,:], '.')
+plt.show()
 
-# sqrt_n = 25
-# n = sqrt_n**2
 
+# # dimension of data
+# d = 2
+# # number of samples
+# n = 25
+# # variance of noise
+# eps2 = 0
+# # determine if we should randomly rotate
+# rotate=False
+
+
+# # 2D manifold in 4D space
+
+# # sqrt_n = 25
+# # n = sqrt_n**2
+
+# # Z = np.zeros((d,n))
+# # for i in range(sqrt_n):
+# # 	for j in range(sqrt_n):
+# # 		x = (-0.5 + (i+1)/sqrt_n)*2*np.pi
+# # 		y = (-0.5 + (j+1)/sqrt_n)*2*np.pi
+# # 		Z[0,j + sqrt_n*i] = x
+# # 		Z[1,j + sqrt_n*i] = y
+# # 		Z[2,j + sqrt_n*i] = np.sin(4*x)
+# # 		Z[3,j + sqrt_n*i] = np.sin(7*y)
+
+
+# # # mean center
+# # Z = Z - np.mean(Z,axis=1,keepdims=True)
+# # # global normalization
+# # Z = Z * n / np.sqrt(np.sum(np.power(Z, 2)))
+
+# # plt.scatter(Z[0,:], Z[1,:])
+# scale=2
 # Z = np.zeros((d,n))
-# for i in range(sqrt_n):
-# 	for j in range(sqrt_n):
-# 		x = (-0.5 + (i+1)/sqrt_n)*2*np.pi
-# 		y = (-0.5 + (j+1)/sqrt_n)*2*np.pi
-# 		Z[0,j + sqrt_n*i] = x
-# 		Z[1,j + sqrt_n*i] = y
-# 		Z[2,j + sqrt_n*i] = np.sin(4*x)
-# 		Z[3,j + sqrt_n*i] = np.sin(7*y)
+# for i in range(n):
+# 	x = (-0.5 + (i+1)/n)*2*np.pi
+# 	Z[0,i] = x
+# 	# Z[1,i] = scale*np.sin(x)
+# 	Z[1,i] = scale*np.sin(x) + scale/2*np.sin(3*x + 1.5)
+
+# # add noise
+# Z = Z + eps2*np.random.randn(d, n)
 
 
-# # mean center
-# Z = Z - np.mean(Z,axis=1,keepdims=True)
-# # global normalization
-# Z = Z * n / np.sqrt(np.sum(np.power(Z, 2)))
+# # center and scale
+# Z = Z - Z.mean(axis=1,keepdims=True)
+# Z = Z * n / np.linalg.norm(Z, 'fro')
+
+# # random rotation, preserves centering and scale
+# if rotate:
+# 	A = np.random.randn(d,d)
+# 	u,s,vt = np.linalg.svd(A)
+# 	Z = u@Z
 
 # plt.scatter(Z[0,:], Z[1,:])
-scale=2
-Z = np.zeros((d,n))
-for i in range(n):
-	x = (-0.5 + (i+1)/n)*2*np.pi
-	Z[0,i] = x
-	# Z[1,i] = scale*np.sin(x)
-	Z[1,i] = scale*np.sin(x) + scale/2*np.sin(3*x + 1.5)
 
-# add noise
-Z = Z + eps2*np.random.randn(d, n)
+# u, s, vt = np.linalg.svd(Z)
+# u_min = u[:,1]
+# print(u_min)
 
+# # scale to look nice on graph
+# u_show = u_min * np.min(np.max(np.abs(Z), axis=1))/2
 
-# center and scale
-Z = Z - Z.mean(axis=1,keepdims=True)
-Z = Z * n / np.linalg.norm(Z, 'fro')
+# plt.scatter(Z[0,:], Z[1,:])
+# plt.quiver(0, 0, u_show[0], u_show[1],scale_units='xy', angles='xy',scale=1)
+# plt.gca().set_aspect('equal', adjustable='box')
+# plt.show()
 
-# random rotation, preserves centering and scale
-if rotate:
-	A = np.random.randn(d,d)
-	u,s,vt = np.linalg.svd(A)
-	Z = u@Z
+# # # Curvature compression on manifold data
+# # 
+# # We now test our new proposed method, curvature compression. The function we theoretically want to optimize for is the following:
+# # 
+# # $$v_* = \argmin_{\|v\|_2 = 1} \max_{x, y \in Z \mid x \ne y} |\langle v, \frac{x-y}{\|x-y\|_2}\rangle|$$
+# # $$= \argmin_{\|v\|_2 = 1} \|\langle v, \frac{x-y}{\|x-y\|_2}\rangle\|_\infty. $$
+# # 
+# # However, since the $L^\infty$ norm is hard to directly optimize, we settle for a smooth surrogate norm: the $L^4$-norm.
 
-plt.scatter(Z[0,:], Z[1,:])
+# # PyTorch model to optimize our custom loss
+# class CurvatureElimination(nn.Module):
 
-u, s, vt = np.linalg.svd(Z)
-u_min = u[:,1]
-print(u_min)
+#     def __init__(self, X, u_0):
+#         super(CurvatureElimination, self).__init__();
+#         # data
+#         self.X = X
+#         # construct edm and gamma weight matrix
+#         gram = X.T@X
+#         edm = torch.diag(gram).reshape((n,1))@torch.ones((1,n)) \
+#                 + torch.ones((n,1))@torch.diag(gram).reshape((1,n)) \
+#                 - 2*gram
 
-# scale to look nice on graph
-u_show = u_min * np.min(np.max(np.abs(Z), axis=1))/2
+#         # diagonal weights don't matter, set to 1 as convention
+#         self.gamma = torch.divide(torch.Tensor([1]),torch.eye(n) + edm)
+#         # projection vec
+#         self.u = nn.Parameter(u_0)
 
-plt.scatter(Z[0,:], Z[1,:])
-plt.quiver(0, 0, u_show[0], u_show[1],scale_units='xy', angles='xy',scale=1)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+#     def forward(self):
+#         U_stacked = torch.ones(self.X.shape)*self.u
+#         A = (self.X.T @ U_stacked - U_stacked.T @ self.X)*self.gamma
+#         return 0.5*A.pow(4).sum()
+# # optimize via projected gradient descent
 
-# # Curvature compression on manifold data
-# 
-# We now test our new proposed method, curvature compression. The function we theoretically want to optimize for is the following:
-# 
-# $$v_* = \argmin_{\|v\|_2 = 1} \max_{x, y \in Z \mid x \ne y} |\langle v, \frac{x-y}{\|x-y\|_2}\rangle|$$
-# $$= \argmin_{\|v\|_2 = 1} \|\langle v, \frac{x-y}{\|x-y\|_2}\rangle\|_\infty. $$
-# 
-# However, since the $L^\infty$ norm is hard to directly optimize, we settle for a smooth surrogate norm: the $L^4$-norm.
+# u_0 = torch.randn((d,1))
+# u_0 = u_0 / torch.sqrt(u_0.pow(2).sum())
+# X = torch.Tensor(Z)
 
-# PyTorch model to optimize our custom loss
-class CurvatureElimination(nn.Module):
+# cc = CurvatureElimination(X, u_0)
+# opt = optim.SGD(cc.parameters(), lr=0.0001)
 
-    def __init__(self, X, u_0):
-        super(CurvatureElimination, self).__init__();
-        # data
-        self.X = X
-        # construct edm and gamma weight matrix
-        gram = X.T@X
-        edm = torch.diag(gram).reshape((n,1))@torch.ones((1,n)) \
-                + torch.ones((n,1))@torch.diag(gram).reshape((1,n)) \
-                - 2*gram
+# for i in range(10000):
+# 	cc.zero_grad()
+# 	# forward call of LinFlow
+# 	loss = cc()
 
-        # diagonal weights don't matter, set to 1 as convention
-        self.gamma = torch.divide(torch.Tensor([1]),torch.eye(n) + edm)
-        # projection vec
-        self.u = nn.Parameter(u_0)
+# 	loss.backward()
 
-    def forward(self):
-        U_stacked = torch.ones(self.X.shape)*self.u
-        A = (self.X.T @ U_stacked - U_stacked.T @ self.X)*self.gamma
-        return 0.5*A.pow(4).sum()
-# optimize via projected gradient descent
+# 	# compute Riemannian gradient
+# 	egrad = cc.u.grad.detach()
+# 	base = cc.u.data.detach()
+# 	base = base / torch.sqrt(base.pow(2).sum())
+# 	rgrad = egrad - base@base.t()@egrad
+# 	# GD step
+# 	opt.step()
+# 	# renormalization step
+# 	with torch.no_grad():
+# 		cc.u.data = cc.u.data / torch.sqrt((cc.u.data).pow(2).sum());
 
-u_0 = torch.randn((d,1))
-u_0 = u_0 / torch.sqrt(u_0.pow(2).sum())
-X = torch.Tensor(Z)
+# 	# determine if we have converged
+# 	gradnorm = torch.linalg.norm(rgrad)
+# 	if gradnorm < 5e-5:
+# 		print(f'converged in {i} steps!')
+# 		break
+# 	if i%100 == 0:
+# 		print(f'g step {i}: {gradnorm}')
 
-cc = CurvatureElimination(X, u_0)
-opt = optim.SGD(cc.parameters(), lr=0.0001)
+# print('done!')
 
-for i in range(10000):
-	cc.zero_grad()
-	# forward call of LinFlow
-	loss = cc()
+# # show learned compression direction
+# u_cc = cc.u.data.detach().clone().numpy()
 
-	loss.backward()
+# print(f'learned compression direction: {u_cc.T}')
+# # scale to look nice on graph
+# u_show = u_cc * np.min(np.max(np.abs(Z), axis=1))/2
 
-	# compute Riemannian gradient
-	egrad = cc.u.grad.detach()
-	base = cc.u.data.detach()
-	base = base / torch.sqrt(base.pow(2).sum())
-	rgrad = egrad - base@base.t()@egrad
-	# GD step
-	opt.step()
-	# renormalization step
-	with torch.no_grad():
-		cc.u.data = cc.u.data / torch.sqrt((cc.u.data).pow(2).sum());
-
-	# determine if we have converged
-	gradnorm = torch.linalg.norm(rgrad)
-	if gradnorm < 5e-5:
-		print(f'converged in {i} steps!')
-		break
-	if i%100 == 0:
-		print(f'g step {i}: {gradnorm}')
-
-print('done!')
-
-# show learned compression direction
-u_cc = cc.u.data.detach().clone().numpy()
-
-print(f'learned compression direction: {u_cc.T}')
-# scale to look nice on graph
-u_show = u_cc * np.min(np.max(np.abs(Z), axis=1))/2
-
-plt.scatter(Z[0,:], Z[1,:])
-plt.quiver(0, 0, u_show[0], u_show[1],scale_units='xy', angles='xy',scale=1)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+# plt.scatter(Z[0,:], Z[1,:])
+# plt.quiver(0, 0, u_show[0], u_show[1],scale_units='xy', angles='xy',scale=1)
+# plt.gca().set_aspect('equal', adjustable='box')
+# plt.show()
--- tools/manifold_generator.py
+++ tools/manifold_generator.py
@@ -1,4 +1,5 @@
 import torch
+from torchdiffeq import odeint
 
 # Class representing a smooth manifold from which we draw data points
 # manifold represented as output of a single smooth function
@@ -16,32 +17,49 @@ class Manifold:
     self.n_basis = n_basis
 
     # Represent functions f(x): R^D -> R with fourier basis
-    # cos(pi*<u,x>) for fixed vector u
+    # a*cos(pi*<u,x>) + b*sin(pi*<v,x>) for fixed vector u
     curv_param = torch.sqrt(torch.Tensor([self.curvature]))
-    self.basis = curv_param*torch.randn(d, D, n_basis)
+    self.basis_cos = torch.randn(D, D, n_basis)
+    self.basis_cos /= (torch.norm(self.basis_cos, dim=0) / curv_param)
+    self.basis_sin = torch.randn(D, D, n_basis)
+    self.basis_sin /= (torch.norm(self.basis_sin, dim=0) / curv_param)
     # coefficients of basis functions, random element on unit sphere
     # of norm D
-    self.coeffs = torch.randn(D, n_basis)
-    self.coeffs /= torch.norm(self.coeffs) / D
+    self.coeffs_cos = torch.randn(D, n_basis)
+    self.coeffs_sin = torch.randn(D, n_basis)
+    self.coeffs_cos /= (torch.norm(self.coeffs_cos) / D)
+    self.coeffs_sin /= (torch.norm(self.coeffs_sin) / D)
+
+  def vecField(self, t, x):
+    # Evaluate the vector field at a set of points x
+    # x: R^(D x N)
+    # returns: v in R^(D x N)
+
+    # Evaluate the basis functions at x
+    # (output is of shape (D, N, n_basis))
+    basis_eval_cos = (self.basis_cos.reshape((self.D, self.D, 1, self.n_basis)) \
+      * x.reshape((self.D, 1, x.shape[1], 1))).sum(dim=0)
+    basis_eval_sin = (self.basis_sin.reshape((self.D, self.D, 1, self.n_basis)) \
+      * x.reshape((self.D, 1, x.shape[1], 1))).sum(dim=0)
+
+    # Evaluate the vector field at x
+    coeffs_reshape_cos = self.coeffs_cos.reshape((self.D, 1, self.n_basis))
+    coeffs_reshape_sin = self.coeffs_sin.reshape((self.D, 1, self.n_basis))
+
+    # final evaluation and sum over basis
+    return (coeffs_reshape_cos*torch.cos(torch.pi * basis_eval_cos) \
+      + coeffs_reshape_sin*torch.sin(torch.pi * basis_eval_sin)).sum(dim=2)
 
   def embedCoordinates(self, psi):
-    # TODO: add sin component
     # Evaluate the manifold at set of coordinates psi
     # psi: R^(d x N)
     # returns:=x in R^(D x N)
-
     N = psi.shape[1]
-
-    # Evaluate the basis functions at psi
-    # (output is of shape (D, N, n_basis))
-    basis_eval = (self.basis.reshape((self.d, self.D, 1, self.n_basis)) \
-      * psi.reshape((self.d, 1, N, 1))).sum(dim=0)
-    print(basis_eval.shape)
-
-    # Evaluate the manifold at psi
-    coeffs_reshape = self.coeffs.reshape((self.D, 1, self.n_basis))
-    print((coeffs_reshape*torch.sin(torch.pi * basis_eval)).sum(dim=2).shape)
-    return (coeffs_reshape*torch.sin(torch.pi * basis_eval)).sum(dim=2)
+    x_psi = torch.zeros(self.D, N)
+    x_psi[:self.d, :] = psi
+    t = torch.tensor([0.0,1])
+    x = odeint(self.vecField, x_psi, t)
+    return x[1,:,:]
 
   def generateSample(self, N, uniform=False):
     # Generate N samples from the manifold
