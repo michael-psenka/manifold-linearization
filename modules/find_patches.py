bc86e5160ff21d3c8353ad01509f03a9f8dc2a65
Druv Pai <druv@druvpai.com>
2022-04-22 13:03:27 -0700

    community detection for neighborhoods

--- modules/find_patches.py
+++ modules/find_patches.py
@@ -1,25 +1,27 @@
 import torch
 
+
 def knn(X):
-	n, d = X.shape
+    n, d = X.shape
 
-	centroids = list()
-	points_in_each_cluster = list()
-	clusters_for_each_point = list(list() for _ in range(n))
+    centroids = list()
+    points_in_each_cluster = list()
+    clusters_for_each_point = list(list() for _ in range(n))
 
-	# stupid way: points are in cluster with their two nearest neighbors
-	# obviously not good, but there for testing if code works with this method
-	pairwise_distances = [(idx1, idx2, torch.linalg.norm(X[idx1] - X[idx2])) for idx1 in range(n) for idx2 in range(n) if idx2 != idx1]
-	key_fn = lambda a: (a[0], a[2])
-	sorted_pairwise_distances = sorted(pairwise_distances, key=key_fn)
-	for i in range(n):
-		neighbor_1 = sorted_pairwise_distances[(n - 1) * i]
-		neighbor_2 = sorted_pairwise_distances[(n - 1) * i + 1]
-		cluster = [X[i], X[neighbor_1[1]], X[neighbor_2[1]]]
-		centroids.append(sum(cluster) / 3.0)
-		points_in_each_cluster.append(cluster)
-		clusters_for_each_point[i].append(i)
-		clusters_for_each_point[neighbor_1[1]].append(i)
-		clusters_for_each_point[neighbor_2[1]].append(i)
+    # stupid way: points are in cluster with their two nearest neighbors
+    # obviously not good, but there for testing if code works with this method
+    pairwise_distances = [(idx1, idx2, torch.linalg.norm(X[idx1] - X[idx2])) for idx1 in range(n) for idx2 in range(n)
+                          if idx2 != idx1]
+    key_fn = lambda a: (a[0], a[2])
+    sorted_pairwise_distances = sorted(pairwise_distances, key=key_fn)
+    for i in range(n):
+        neighbor_1 = sorted_pairwise_distances[(n - 1) * i]
+        neighbor_2 = sorted_pairwise_distances[(n - 1) * i + 1]
+        cluster = [X[i], X[neighbor_1[1]], X[neighbor_2[1]]]
+        centroids.append(sum(cluster) / 3.0)
+        points_in_each_cluster.append(cluster)
+        clusters_for_each_point[i].append(i)
+        clusters_for_each_point[neighbor_1[1]].append(i)
+        clusters_for_each_point[neighbor_2[1]].append(i)
 
-	return centroids, points_in_each_cluster, clusters_for_each_point
+    return centroids, points_in_each_cluster, clusters_for_each_point
---
+++ modules/find_patches_community_detection.py
@@ -0,0 +1,48 @@
+import networkx as nx
+import networkx.algorithms.community as nx_comm
+import torch
+
+
+class CommunityDetection:
+    def __init__(self, X: torch.Tensor, eps: float = 1e-8):
+        """
+        Builds KNN graph, where k = O(sqrt(n)). Vertices are n data points xi, edges are 1/d(xi, xj)
+        if xj is one of the k nearest neighbors of xi.
+
+        :param X: data matrix, (n, d)
+        :param eps: perturbation to avoid divide-by-zero error, default=1e-8 should be good
+        """
+        n, d = X.shape
+        k = min(int(n ** 0.5) + 1, X.shape[0])
+        pairwise_distances = torch.cdist(X, X)
+        sorted_pairwise_distances, sorted_indices = torch.sort(pairwise_distances, dim=1)
+        sorted_pairwise_distances = sorted_pairwise_distances.tolist()
+        sorted_indices = sorted_indices.tolist()
+
+        self.knn_graph = nx.Graph()
+        for i in range(n):
+            for j in range(1, k):
+                self.knn_graph.add_edge(i, sorted_indices[i][j], weight=1/(sorted_pairwise_distances[i][j] + eps))
+
+    def find_communities(self):
+        """
+        Finds the communities in the KNN graph via Clauset-Newman-Moore greedy modularity maximization.
+        How this works: lots of points which are very close to each other are considered a "community", and
+        regions which are far away are considered different communities. Finds best number of communities
+        and partitions the vertices into these communities.
+
+        NOTE: this is strict partition, no overlap.
+
+        :return: a list of index sets where each index set is a community
+        """
+        return nx_comm.greedy_modularity_communities(self.knn_graph)
+
+
+def find_patches(X: torch.Tensor):
+    """
+    Finds the neighborhoods in X. Neighborhoods are disjoint for now.
+
+    :param X: data matrix, (n, d)
+    :return: a list of index sets I_k where each I_k is a community and U I_k = {1, ..., n}
+    """
+    return CommunityDetection(X).find_communities()
