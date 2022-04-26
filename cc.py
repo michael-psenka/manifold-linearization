5445a9fa2b0d065af9190b5a941182d0a6149b4a
Michael Psenka <michael.psenka@gmail.com>
2022-04-26 11:37:09 -0700

    big restructure midway

--- cc.py
+++ cc.py
@@ -6,12 +6,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from modules import find_patches, flatten_patches
-from modules import cc_nn
+from modules import cc_nn, find_patches, flatten_patches, \
+	merge_patches, pca_init
 # ****************************i*************************************************
 # This is the primary script for the curvature compression algorithm.
 # Input: data matrix X of shape (D,n), where D is the embedding dimension and
-# n is the number of data points; OPTIONAL: N of shape (D,k) are k center
+# n is the number of data points; OPTIONAL: Ne of shape (D,k) are k center
 # points determining neighborhoods, and E of shape (k,k) a binary matrix
 # denoting which neighborhoods are connected.
 
@@ -23,18 +23,67 @@ from modules import cc_nn
 	# 2. find global flattening
 # later, we will want to iteratively merge patches
 
-def cc(X, N=-1, E=-1):
-	# STEP 1: if not specified, determine appropriate neighborhoods for
-	# local linearization
-	# see modules/find_patves.py for details
-	if N == -1:
-		N, E = find_patches.knn(X)
+def cc(X, Np=-1, E=-1):
+	# HYPERPARAMTERs
+	_gamma = 1
+	# set up needed variables
+	d, N = X.shape
+	mu = X.mean(axis=1, keepdim=True)
 
-	# STEP 2: compute the local linearization within each neighborhood
-	U, alpha = flatten_patches.local_linearization(X, N, E)
-	# STEP 3: compute the global flattening
-	u_flat = flatten_patches.align_patches(U)
-	# STEP 4: construct the neural network
-	f = cc_nn.CC_2Network(U, alpha, u_flat)
+	cc_network = cc_nn.CCNetwork(mu)
+	# STEP 0: use PCA to project out machine-precision directions
+	X, U_r = pca_init(X - mu)
+	# add PCA operation to network
+	cc_network.add_module("pca", cc_nn.LinearCol(U_r.T))
+	# new ambient dimension, rank of PCA
+	d_current = U.shape[1]
+
+
+	# MAIN LOOP: looping through ambient dimension d_current until
+	# it has been reduced as much as possible
+	has_converged = False
+	while not has_converged:
+		# STEP 1: find patches
+		Np, E = find_patches(X)
+
+		# INNER LOOP: flatten and merge patches
+		k = Np.shape[1]
+
+		# our first flattening is from the points within each neighborhood
+		# we also check here to see if the manifold has been flattened
+		# everywhere -- if so, impossible to compress further
+		U, alpha, has_converged = flatten_patches.flatten_from_points(X, Np, E)
+		# if test for convergence is true, we don't want to add the above
+		# generated layer
+		if has_converged:
+			break
+
+		# add layer to network
+		cc_layer = cc_nn.CCLayer(Np, U, alpha, _gamma)
+		# add to network
+		cc_network.add_operation(cc_layer)
+
+		# if k > 1, linearize and merge neighborhoods until we merge into
+		# one neighborhood
+		if k > 1:
+			while k > 1:
+				# merge patches
+				# M is a structure that contains information on what clusters
+				# we merge patches into
+				M, Np_new, E_new = merge_patches(Np, E)
+				# flatten patches
+				U_new, alpha = flatten_patches.flatten_from_normals(X, U, M, Np_new, E_new)
+				# STEP 3: construct global map from local flattenings
+				cc_layer = cc_nn.CCLayerMulti(Np_new, U_new, alpha, _gamma)
+				# add to network
+				cc_network.add_operation(cc_layer)
+				
+				# update parameters
+				k = Np.shape[1]
+				Np = Np_new
+				E = E_new
+				U = U_new
+
+			flatten_patches.flatten_from_normals(X, N, E)
 	
-	return f
+	return cc_network
--- modules/cc_nn.py
+++ modules/cc_nn.py
@@ -1,45 +1,90 @@
-# Python class that represents a layer of a neural network
+
+
+import torch
+from torch import nn
+
+# Python class that represents a layer of a neural network when merging
+# multiple neighborhoods
 # parameters:
 # Np: (neighborhood points) point set of shape (D,k) representing k points in R^D of
 # center of neighborhoods
 # A: vector set of shape (D,k) representing normal directions for affine projector
 # alpha: scalar set of shape (1,k) representing offset for affine projector
 # gamma: scalar multiple for softmax
-
-import torch
-class CCLayer:
-	def __init__(self, Np, A, alpha, gamma):
+class CCLayer(nn.Module):
+	def __init__(self, Np, U, alpha, gamma):
+		super(CCLayer, self).__init__()
 		self.Np = Np
-		self.A = A
+		self.U = U
 		self.alpha = alpha
 		self.gamma = gamma
 		self.D = Np.shape[0]
 		self.k = Np.shape[1]
 		
-	def forward(self, x):
-		# x: R^(D x N)
+	def forward(self, X):
+		# X: R^(D x N)
 		# returns: R^(D x N)
-		N = x.shape[1]
+
+
+		# if only one neighborhood, simply global projection operator
+		if self.k == 1:
+			return 
+		N = X.shape[1]
 
 		# vector offsets for affine projectors
-		b = self.A*self.alpha.reshape((1,self.k))
+		b = self.U*self.alpha.reshape((1,self.k))
 		b = b.reshape((self.D,1,self.k))
+		
+		# reshaped X for broadcasting operations
+		X_reshape = X.reshape((self.D, N, 1))
 		
 		# Compute the affine projection
 
 		# (output is of shape (N, k))
-		uTx = (self.A.reshape((self.D, 1, self.k)) \
-			* x.reshape((self.D, N, 1))).sum(dim=0)
+		uTX = (self.U.reshape((self.D, 1, self.k)) \
+			* X_reshape).sum(dim=0)
 		# (output is of shape (D, N, k))
-		uuTx = self.A.reshape((self.D, 1, self.k)) \
-			* uTx.reshape((1, N, self.k))
+		uuTX = self.U.reshape((self.D, 1, self.k)) \
+			* uTX.reshape((1, N, self.k))
 
-		affine_proj = x.reshape((self.D, self.N, 1)) - uuTx + b
-		
-		# Compute the softmax
+		affine_proj = X_reshape - uuTX + b
+
+		# Computes probability of each point in batch being in each neighborhood
 		# (output is of shape (N, k))
-		softmax = torch.softmax(self.gamma * (affine_proj + self.b), dim=1)
+		Np_reshape = self.Np.reshape((self.D, 1, self.k))
+		softmax = torch.softmax(self.gamma * (Np_reshape - X_reshape).pow(2).sum(axis=0), dim=1)
 		
 		# Compute the output
 		# (output is of shape (D, N))
-		return (self.N.reshape((self.D, 1, self.k)) * softmax.reshape((1, self.k, 1))).sum(dim=2)
+		return (affine_proj * softmax.reshape((1, N, self.k))).sum(dim=2)
+
+# we will iteratively build this network through calls to Sequential.add_module:
+# e.g. ccn = CCNetwork(); ccn.network.add_module("layer1", CCLayer(Np, U, alpha, gamma))
+
+# mu is a tensor of shape (D, 1) and corresponds to the mean of the data
+# we need this since CCNetwork is constructed from the centered data, so given a new
+# data point, we need to offset by the measured mean for our network to agree on the
+# training data
+class CCNetwork(nn.Module):
+	def __init__(self, mu):
+		super(CCNetwork, self).__init__()
+		self.network = torch.nn.Sequential()
+		self.mu = mu
+		
+	def forward(self, X):
+		X_center = X - self.mu
+		return self.network(X_center)
+
+	def add_operation(self, nn_module):
+		self.network.add_module(f'layer {len(self.network)}', nn_module)
+
+
+# linear layer as in PyTorch, but with matmul convention flipped. here:
+# y = A@x, instead of y = x@A^T
+class LinearCol(nn.Module):
+	def __init__(self, A):
+		super(LinearCol, self).__init__()
+		self.A = nn.Parameter(A)
+		
+	def forward(self, x):
+		return self.A@x
