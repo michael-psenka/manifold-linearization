08f7cc64f6af78dcccae7fd7a7cc8affe48cabf6
Michael Psenka <michael.psenka@gmail.com>
2022-05-09 14:19:05 -0700

    v1.0 running!

--- cc.py
+++ cc.py
@@ -55,13 +55,7 @@ def cc(X, k):
 	ind_Z, merges, A_N, mu_N, G_N = \
 		find_patches_community_detection.find_neighborhood_structure(Z, k, _eps, _eps_N)
 
-	# test membership layer
-	print('Finding membership...')
-	layer_pi = cc_nn.CCUpdatePi(_gamma, A_N, mu_N)
-	print('Applying membership...')
-	ZPi = layer_pi(Z)
-	cc_network.add_operation(layer_pi, f"pi;d:{d_current}")
-
+	p = len(ind_Z)
 	# test flatten from points
 
 	print('Beginning main construction loop.')
@@ -69,50 +63,54 @@ def cc(X, k):
 	# it has been reduced as much as possible
 	has_converged = False
 	while not has_converged:
+		print('---------- GLOBAL STEP ----------')
+		# STEP 1: update memberships
+		print('Finding membership...')
+		layer_pi = cc_nn.CCUpdatePi(_gamma, A_N, mu_N)
+		ZPi = layer_pi(Z)
+		cc_network.add_operation(layer_pi, f"pi;d:{d_current}")
 
-		# STEP 1: find init normal directions from neighborhood points
+		# STEP 2: find init normal directions from neighborhood points
 		# here we optimize for injectivity with the minimal induced extrinsic curvaturew
+
+		# Note that ZPi[:d_current,:] is Z, and ZPi[d_current:,:] is Pi
 		print('Finding normals...')
-		Z = ZPi[:d_current,:]
-		U = flatten_patches.flatten_from_points(Z, ind_Z, G_N[0])
+		U = flatten_patches.flatten_from_points(Z[:d_current,:], ind_Z, G_N[0])
 
 		print('Aligning projectors...')
 		alpha = flatten_patches.align_offsets(ZPi, U)
 
-		print('Flatten from normals...')
-		U2 = flatten_patches.flatten_from_normals(U, merges[0], G_N[1])
+		cclayer = cc_nn.CCLayer(U, alpha)
+		ZPi = cclayer(ZPi)
+		cc_network.add_operation(cclayer, f"lin-base;d:{d_current}")
+
+		# STEP 3: merge and flatten neighborhoods through normal directions
+		U_base = U
+		for i in range(len(merges)):
+			print('Flatten from normals...')
+			U = flatten_patches.flatten_from_normals(U_base, merges[i], G_N[i+1])
+
+			# if this is the last iteration, we know it's just a global projection
+			if i == len(merges) - 1:
+				cclayer = cc_nn.LinearProj(U, d_current)
+				ZPi = cclayer(ZPi)
+				cc_network.add_operation(cclayer, f"lin-proj-global;d:{d_current}")
+			else:
+				# update U_base
+				for j in range(len(merges[i])):
+					U_base[:,merges[i][j]] = U[:,[j]]
+
+				print('Aligning projectors...')
+				alpha = flatten_patches.align_offsets(ZPi, U_base)
+				print('Creating and applying layer...')
+				cclayer = cc_nn.CCLayer(U_base, alpha)
+				# note only Z is affected here, we just need Pi in
+				ZPi = cclayer(ZPi)
+				cc_network.add_operation(cclayer, f"lin-normals-{i+1};d:{d_current}")
 
 		# if test for convergence is true, we don't want to add the above
 		# generated layer
 		if has_converged:
 			break
-		
-		# add layer to network
-		cc_layer = cc_nn.CCLayer(Np, U, alpha, _gamma)
-		# add to network
-		cc_network.add_operation(cc_layer)
-
-		# if k > 1, linearize and merge neighborhoods until we merge into
-		# one neighborhood
-		if k > 1:
-			while k > 1:
-				# merge patches
-				# M is a structure that contains information on what clusters
-				# we merge patches into
-				M, Np_new, E_new = merge_patches(Np, E)
-				# flatten patches
-				U_new, alpha = flatten_patches.flatten_from_normals(X, U, M, Np_new, E_new)
-				# STEP 3: construct global map from local flattenings
-				cc_layer = cc_nn.CCLayerMulti(Np_new, U_new, alpha, _gamma)
-				# add to network
-				cc_network.add_operation(cc_layer)
-				
-				# update parameters
-				k = Np.shape[1]
-				Np = Np_new
-				E = E_new
-				U = U_new
-
-			flatten_patches.flatten_from_normals(X, N, E)
 	
 	return cc_network
