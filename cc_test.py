09cf822fb0cbb15a7217dec9d1afe1a5dd9c5639
Michael Psenka <michael.psenka@gmail.com>
2022-09-08 00:08:01 -0400

    overhauled to use brent's dcargs library, now cli

--- cc_test.py
+++ cc_test.py
@@ -1,3 +1,5 @@
+import sys
+
 # %matplotlib widget
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,50 +13,105 @@ import torchvision.transforms as transforms
 
 import cc
 
-# download dataset
-transform = transforms.Compose(
-    [transforms.Resize(32),
-     transforms.ToTensor(),
-     transforms.Normalize(0.5, 0.5)])
-dataset = datasets.MNIST(root='./torch-dataset', train=True,
-                         download=True, transform=transform)
+# magic argparser library thank you github.com/brentyi
+import dataclasses
+import dcargs
 
-print('Loading data...')
-# ############ MNIST
-# load dataset into pytorch
-# data_loader = torch.utils.data.DataLoader(dataset, batch_size=600000)
-# data,labels = next(iter(data_loader))
-# data = data.cuda()
+##### COMMAND LINE ARGUMENTS #####
+# run cc_test.py --help to get help text
+# for each argument below, underscores become dashes
+# for example, cc.py --n_iters 1000 is invalid, but
+# cc.py --n-iters 1000 is valid
 
-# # select single class of dataset
-# Z = data[labels==2]
-# Z = Z.reshape((5958,32**2))
-# Z = Z.T
+@dataclasses.dataclass
+class Args:
+	"""cc_test.py
+	A python script for testing out the Curvature Compression algorithm."""
 
-# ############ dummy data
-# Z = torch.randn(10,1000)
-d = 2
-n = 50
-Z =torch.zeros((n,d))
-for i in range(n):
-	x = (1.5 - 1.4/2 +1.4*(i+1)/n)*torch.pi
-	Z[i,0] = torch.cos(torch.tensor(x))
-	# Z[1,i] = scale*np.sin(x)
-	Z[i,1] = torch.sin(torch.tensor(x))
+	dataset: str = "sine-wave"
+	"""A string indicating which dataset to use.
+	 Run "python cc_test.py --get-datasets" for a list of possible datasets."""
+
+	get_datasets: bool = False  # If true, print out a list of available datasets and exit.
+
+	N: int = 100
+	""" Number of training datapoints to use. Note this will not effect some
+	 datasets, such as MNIST """
+
+	n_iters: int = 1000 # Number of iterations to run the CC algorithm for
+
+	d_target: int = 1 # dimension we want to flatten the data to
+
+	gamma_0: float = 1
+	""" starting value of the "inverse neighborhood size", in the sense that:
+	# smaller values of gamma_0 correspond to larger neighborhood sizes, and vice versa """
+
+# NOTE: if adding a new dataset, add it to the list below as well as the switch statement
+# inside the main function
+datasets = {
+  "sine wave": "The graph of a single period sine wave embedded in 2D",
+  "semicircle": "A semicircle of 1.5pi radians embedded in 2D",
+  "MNIST": """A single class of the MNIST dataset (in our case, the 2's). This is in spirit to the
+		\"union of manifolds\" hypothesis""",
+}
+
+if __name__ == "__main__":
+
+	# parse command line arguments
+	args = dcargs.cli(Args)
+
+	if args.get_datasets:
+		print("Available datasets:\n")
+		for dataset, desc in datasets.items():  # dct.iteritems() in Python 2
+			print(f"{dataset}: {desc} \n")
+		exit(0)
+
+	# Load the proper dataset
+	if args.dataset == "sine-wave":
+		N = args.N
+		D = 2
+		X = torch.zeros((N,D))
+
+		for i in range(N):
+			x_coord = i / N*2*torch.pi
+			X[i,0] = x_coord
+			X[i,1] = np.sin(x_coord)
+
+	elif args.dataset == "semicircle":
+		N = args.N
+		D = 2
+		X = torch.zeros((N,D))
+
+		for i in range(N):
+			theta_coord = i / N*1.5*torch.pi
+			X[i,0] = np.cos(theta_coord)
+			X[i,1] = np.sin(theta_coord)
+
+	elif args.dataset == "MNIST":
+		dataset = datasets.MNIST(root='./torch-dataset', train=True,
+								download=True)
+
+		# load dataset into pytorch
+		data_loader = torch.utils.data.DataLoader(dataset, batch_size=600000)
+		data,labels = next(iter(data_loader))
+		data = data.cuda()
+
+		# select single class of dataset
+		X = data[labels==2]
+		X = X.reshape((5958,32**2))
+		X = X.T
+	else:
+		sys.exit('Invalid dataset. Run "python cc_test.py --get-datasets" for a list of possible datasets.')
 
 
-# center and scale
-Z_mean_orig = Z.mean(axis=1, keepdim=True)
-Z = Z - Z_mean_orig
-Z_norm_orig = np.linalg.norm(Z, 'fro')
-Z = Z * n / Z_norm_orig
-print('Starting CC!')
-# main training command
-f = cc.cc(Z, d_desired=1)
+	# center and scale data
+	X_mean = torch.mean(X, dim=0)
+	X = X - X_mean
+	X_norm = torch.norm(X)/np.sqrt(N*D) #expeded norm of gaussian is sqrt(D)
+	X = X / X_norm
 
-Z_new = f(Z).detach().numpy()
 
-plt.plot(Z[0,:], Z[1,:], '.')
-plt.show()
-plt.plot(Z_new[0,:], Z_new[1,:], '.', c='r')
-plt.show()
+	Z = cc.cc(X, d_target=args.d_target, gamma_0=args.gamma_0, n_iters=args.n_iters)
+
+	plt.plot(Z[:,0], Z[:,1], '.')
+	plt.show()
