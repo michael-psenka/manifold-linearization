5c580c777b8253f38238ee476f8e83861abc3f4a
Michael Psenka <michael.psenka@gmail.com>
2022-09-25 20:59:07 -0400

    overhaul, now works for arbitrary dimensions

--- cc.py
+++ cc.py
@@ -30,9 +30,10 @@ def cc(X):
 	#######	## HYPERPARAMTERS ####
 	##############################
 
-	# how many iterations to binary search for biggest possible radius
-	fid = 10
-	# number of flattening steps to perform (currently no stoppic criterion)
+	# how many times of no progress do we call convergence?
+	n_stop_to_converge = 5
+	converge_counter = 0
+	# number of flattening steps to perform
 	n_iter = 200
 	# how many max steps for inner optimization of U, V
 	# (stopping criterion implemented)
@@ -46,6 +47,19 @@ def cc(X):
 	# minimum radius for kernel
 	r_ratio_min = 0.3
 
+	EDM_X = torch.cdist(X, X,p=2)
+	# radius for checking dimension. Should be as small as possible,
+	# but big enough that at every point there's at least one sample
+	# along every intrinsic dimension
+	r_dimcheck = 0.25*EDM_X.max()
+	# minimum allowed radius for each flattening
+	# want this to be relatively larger to converge to flat
+	# representation faster
+	r_min = 0.25*EDM_X.max()
+	# maximum allowed radius for each flattening
+	r_max = 1.1*EDM_X.max()
+	# minimum step size for determining optimal radius
+	r_step_min = EDM_X[EDM_X > 0].min()
 
 	############# INIT GLOBAL VARIABLES##########
 	# encoder network
@@ -55,107 +69,193 @@ def cc(X):
 	Z = X.clone()
 	# ################ MAIN LOOP #########################
 	with trange(n_iter, unit="iters") as pbar:
-		for _ in pbar:
-
-			# for checking convergence
-			did_shrink = False
-
-			# start our "neighborhood size" at max possible
-			d_max = torch.cdist(Z, Z).max()
-			d_init = d_max.clone() / 2
-			d_curr = d_init.clone()
-
-			# 1 step of flattening/regeneration
+		for j in pbar:
+			# STEP 0: stochastically choose center of the neighborhood to
+			# flatten and reconstruct
 			choice = torch.randint(N, (1,))
-			# choice=25
 			z_c = Z[choice,:]
 
-			U_0 = torch.randn(2, 1)
-			U_0 = U_0 / torch.norm(U_0, p=2)
-			U = torch.nn.Parameter(U_0.clone())
-			U.requires_grad = True
+			# STEP 1: find minimal dimension d we can flatten neighborhood
+			# to and still be able to reconstruct
 
+			# note d is implicitly returned, as U, V are of shape (D, d)
+			U, loss_rdimcheck = find_d(Z, z_c, r_dimcheck, n_iter_inner)
+			# STEP 2: use secant method to find maximal radius that achieves
+			# desired reconstruction loss
 
-			opt_U = optim.SGD([U], lr=1)
-		# outer loop, searching for max radius with desired fidelity
-			for i in range(fid):
-				# set up kernel
-				gamma = log2/d_curr**2
-				kernel_pre = torch.exp(-gamma*(Z - z_c).pow(2).sum(dim=1, keepdim=True))
-				z_mu = (Z*kernel_pre).sum(dim=0, keepdim=True) / kernel_pre.sum()
-				kernel = torch.exp(-gamma*(Z - z_mu).pow(2).sum(dim=1, keepdim=True))
+			# get needed second observation
+			U, V, loss_rmidpoint = opt_UV(Z, z_c, U, n_iter_inner, r=(r_min + r_max)/2)
 
-				U_old = U.data.clone()
-				# inner loop, find best U, V to reconstruct
-				for _ in range(n_iter_inner):
-					opt_U.zero_grad()
-					# opt_V.zero_grad()
-					# opt_alpha.zero_grad()
-					coord = (Z - z_c) @ U
-					coord2 = coord.pow(2)
-					Z_perp = (Z - z_c) - coord @ U.T
-					A = coord2 * kernel
-					b = Z_perp * kernel
+			# begin secant method (note we use log loss for numerical reasons)
+			log_thres_recon = torch.log(torch.Tensor([thres_recon]))
+			r_m2 = r_dimcheck
+			f_m2 = torch.log(loss_rdimcheck) - log_thres_recon
+			r_m1 = (r_min + r_max)/2
+			f_m1 = torch.log(loss_rmidpoint) - log_thres_recon
 
-					# least squares solution for V, note automatically orthogonal to U
-					# with torch.no_grad():
-					V = (torch.linalg.pinv(A)@b).T
+			while torch.abs(r_m1 - r_m2) > r_step_min:
 
-					loss = 0.5*(kernel*(Z_perp - coord2@V.T)).pow(2).mean()
-					# loss = (U).pow(2).mean()
-					loss.backward()
+				# threshold denominator for numerical stability
+				f_diff = f_m1 - f_m2
+				if torch.abs(f_diff) < 1e-6:
+					if f_diff >= 0:
+						f_diff = 1e-6
+					else:
+						f_diff = -1e-6
 
-					opt_U.step()
-					with torch.no_grad():
-						U.data = U.data / torch.norm(U.data, p=2)
-						step_size = (U.data - U_old).pow(2).mean().sqrt()
-						U_old = U.data.clone()
+				r = r_m1 - (r_m1 - r_m2)/f_diff*f_m1
 
-						if step_size < 1e-5:
-							break
+				# if we reach either boundary, threshold and exit
+				if r < r_min:
+					r = r_min
+				elif r > r_max:
+					r = r_max
+
+				U, V, loss_r = opt_UV(Z, z_c, U, n_iter_inner, r=r)
+				f_r = torch.log(loss_r) - log_thres_recon
+
+				r_m2 = r_m1.clone()
+				f_m2 = f_m1.clone()
+				r_m1 = r.clone()
+				f_m1 = f_r.clone()
 				
-				# achieved desired recon loss, so increase neighborhood
-				# size
 
-				# threshold at min radius, tune step size alpha
-				# to achieve reconstruction loss
-				if d_curr / d_max < r_ratio_min:
-					break
-					
-
-				if loss.item() <= thres_recon:
-					d_curr += d_init * (0.5)**(i+1)
-				# didn't achieve desired recon loss, so decrease neighborhood
-				else:
-					d_curr -= d_init * (0.5)**(i+1)
-					did_shrink = True
-
-				# if radius already bigger than biggest possible, no need to continue searching
-				if d_curr > d_max:
-					break
-			
-
-			# step size small enough to achieve reconstruction loss, and also
-			# small enough to keep output manifold smooth
-			alpha = float(min(alpha_max, np.sqrt(thres_recon/loss.item())))
+			# STEP 3: line search for biggest alpha that gets us to desired fidelity
+			alpha = float(min(alpha_max, np.sqrt(thres_recon / loss_r.item())))
+			# STEP 4: add layer to network
 			Z = Z.detach()
-			U = U.detach()
-			V = V.detach()
+			U = U.detach().clone()
+			V = V.detach().clone()
 
-			# MAIN CONVERGENCE: is neighborhood size global and reconstruction linear?
-			if not did_shrink and V.pow(2).mean().sqrt().item() < 1e-4:
-				break
+			gamma = float(np.log(2))/(r.item()**2)
+			kernel_pre = torch.exp(-gamma*(Z - z_c).pow(2).sum(dim=1, keepdim=True))
+			z_mu = (Z*kernel_pre).sum(dim=0, keepdim=True) / kernel_pre.sum()
 
 			f_layer = cc_nn.FLayer(U, z_mu, gamma, alpha)
 			g_layer = cc_nn.GLayer(U, V, z_mu, z_c, gamma, alpha)
 
-			f.add_operation(f_layer)
-			g.add_operation(g_layer)
-			# update data
-			Z = f_layer(Z)
+			# test for convergence
+			Z_new = f_layer(Z)
+			if (Z_new - Z).pow(2).mean().sqrt() < 1e-4:
+				converge_counter += 1
+				if converge_counter >= n_stop_to_converge:
+					break
+			else:
+				converge_counter = 0
+				f.add_operation(f_layer)
+				g.add_operation(g_layer)
+				# only update representation if we add the layer
+				Z = Z_new.clone()
 
+			
 			with torch.no_grad():
 				recon_loss = 0.5*(g(Z) - X).pow(2).mean()
-			pbar.set_postfix({"loss": loss.item(), "d_ratio": (d_curr/d_max).item(), "alpha": alpha, "recon_loss": recon_loss.item()})
+			pbar.set_postfix({"local_recon": loss_r.item(), \
+				"global_recon": recon_loss.item(), "d": U.shape[1], \
+					"r_ratio": (r/r_max).item(), "alpha": alpha})
 
 	return f, g
+
+
+
+# ################## HELPER METHODS #####################
+
+def opt_UV(Z, z_c, U_0, n_iter_inner, r=-1, kernel=-1):
+	# optimize U, V
+	U = torch.nn.Parameter(U_0.clone())
+	U.requires_grad = True
+
+	opt_U = optim.SGD([U], lr=1)
+
+	# must specify either r or kernel
+	if type(r) != torch.Tensor and type(kernel) != torch.Tensor:
+		raise ValueError('Must specify either r or kernel')
+
+	# init kernel
+	if type(kernel) != torch.Tensor:
+		gamma = float(np.log(2))/(r**2)
+		kernel_pre = torch.exp(-gamma*(Z - z_c).pow(2).sum(dim=1, keepdim=True))
+		z_mu = (Z*kernel_pre).sum(dim=0, keepdim=True) / kernel_pre.sum()
+		kernel = torch.exp(-gamma*(Z - z_mu).pow(2).sum(dim=1, keepdim=True))
+	
+	# optimize U
+	for i in range(n_iter_inner):
+		U_old = U.data.clone()
+		opt_U.zero_grad()
+		# opt_V.zero_grad()
+		# opt_alpha.zero_grad()
+		coord = (Z - z_c) @ U
+		Z_perp = (Z - z_c) - coord @ U.T
+		coord2 = coord.pow(2)
+		A = coord2 * kernel
+		b = Z_perp * kernel
+
+		# least squares solution for V, note automatically orthogonal to U
+		with torch.no_grad():
+			V = ((A.T@A).inverse() @ (A.T@b)).T
+
+		loss = 0.5*(kernel*(Z_perp - coord2@V.T)).pow(2).mean()
+		# loss = (U).pow(2).mean()
+		loss.backward()
+
+		opt_U.step()
+		# update to gradient
+
+		with torch.no_grad():
+			
+			# project onto Steifel manifold
+			if U.data.shape[1] == 1:
+				U.data = U.data / torch.norm(U.data, p=2)
+			else:
+				U_svd, S_svd, Vh_svd = torch.linalg.svd(U.data, full_matrices=False)
+				U.data = U_svd@Vh_svd
+
+			step_size = (U.data - U_old).pow(2).mean().sqrt()
+			U_old = U.data.clone()
+
+			if step_size < 1e-5:
+				break
+			
+	loss_final = 0.5*(kernel*(Z_perp - coord2@V.T)).pow(2).mean()
+	return U.detach().data, V.detach().data, loss_final.detach()
+
+def find_d(Z, z_c, r_dimcheck, n_iter_inner):
+	# We find the minimial d by iteratively fitting a model
+	# for some size d, then increase d and repeat if the max
+	# reconstruction error is too large
+
+	max_error_ratio = 0.4
+
+	N, D = Z.shape
+	# init
+	U_0 = torch.randn(D, 1)
+	U_0 = U_0 / torch.norm(U_0, p=2)
+
+	# init tracking variable
+	max_error = max_error_ratio*r_dimcheck
+
+	# note kernel will stay the same for all d
+	gamma = float(np.log(2))/(r_dimcheck**2)
+	kernel_pre = torch.exp(-gamma*(Z - z_c).pow(2).sum(dim=1, keepdim=True))
+	z_mu = (Z*kernel_pre).sum(dim=0, keepdim=True) / kernel_pre.sum()
+	kernel = torch.exp(-gamma*(Z - z_mu).pow(2).sum(dim=1, keepdim=True))
+
+	while max_error >= max_error_ratio*r_dimcheck:
+		U, V, loss = opt_UV(Z, z_c, U_0, n_iter_inner, kernel=kernel)
+
+		# compute max error, if too large, increase d
+		coord = (Z - z_c) @ U
+		Z_perp = (Z - z_c) - coord @ U.T
+		coord2 = coord.pow(2)
+		max_error = (kernel*(Z_perp - coord2@V.T)).norm(dim=1).max()
+		# print(f'current max error: {max_error}')
+		# print(f'max error thredhold: {max_error_ratio*r_dimcheck}')
+		if max_error >= max_error_ratio*r_dimcheck:
+			# add new vec , orthogonalize via gram schmidt
+			U_new = torch.randn(D, 1)
+			U_new = U_new - U @ (U.T@U_new)
+			U_new = U_new / torch.norm(U_new, p=2)
+			U_0 = torch.cat((U, U_new), dim=1).clone()
+
+
+	return U.detach().data, loss.detach().data
