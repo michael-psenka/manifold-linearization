5e0056a567581090aea53233194818c54f179637
Michael Psenka <michael.psenka@gmail.com>
2022-10-12 22:12:07 -0400

    performance overhaul: find_d, restrict to batch, lstsq

--- cc.py
+++ cc.py
@@ -34,7 +34,7 @@ def cc(X):
 	##############################
 
 	# how many times of no progress do we call convergence?
-	n_stop_to_converge = 3
+	n_stop_to_converge = 5
 	converge_counter = 0
 	# number of flattening steps to perform
 	n_iter = 300
@@ -47,23 +47,31 @@ def cc(X):
 	# global parameter for kernel
 	alpha_max = 0.5
 
+	# used to define hyperparameters
 	EDM_X = torch.cdist(X, X,p=2)
+	edm_max = EDM_X.max()
+	edm_min = EDM_X[EDM_X > 0].min()
+
 	# radius for checking dimension. Should be as small as possible,
 	# but big enough that at every point there's at least one sample
 	# along every intrinsic dimension
-	r_dimcheck = 0.2*EDM_X.max()
+	r_dimcheck = 0.2 * edm_max
 	# minimum allowed radius for each flattening
 	# want this to be relatively larger to converge to flat
 	# representation faster
-	r_min = 0.2*EDM_X.max()
+	r_min = 0.2* edm_max
 	# maximum allowed radius for each flattening
-	r_max = 1.1*EDM_X.max()
+	r_max = 1.1 * edm_max
 	# 2nd radius to check for secant optimization
-	r_midpoint = 0.7*r_min + 0.3*r_max
+	r_midpoint = 0.5 * (r_min + r_max)
 	# minimum step size for determining optimal radius
-	r_step_min = 0.9*EDM_X[EDM_X > 0].min() + 0.1*EDM_X.max()
+	r_step_min = edm_min
 	# max steps to take when finding biggest r
-	n_iter_rsearch = 3
+	n_iter_rsearch = 1
+
+	# #################### TRACKING VARIABLES ####################
+	# track previous intrinsic dimension to avoid redundant searches
+	d_prev = 1
 
 	############# INIT GLOBAL VARIABLES##########
 	# encoder network
@@ -83,12 +91,12 @@ def cc(X):
 			# to and still be able to reconstruct
 
 			# note d is implicitly returned, as U, V are of shape (D, d)
-			U, loss_rdimcheck = find_d(Z, z_c, r_dimcheck, n_iter_inner)
+			U, loss_rdimcheck = find_d(Z, z_c, r_dimcheck, n_iter_inner, d_prev)
 			# STEP 2: use secant method to find maximal radius that achieves
 			# desired reconstruction loss
 
 			# get needed second observation
-			U, V, loss_rmidpoint = opt_UV(Z, z_c, U, n_iter_inner, r=(r_min + r_max)/2)
+			U, V, loss_rmidpoint = opt_UV(Z, z_c, U, n_iter_inner, r=r_midpoint)
 
 			# begin secant method (note we use log loss for numerical reasons)
 			log_thres_recon = torch.log(torch.Tensor([thres_recon]))
@@ -124,7 +132,7 @@ def cc(X):
 				f_m1 = f_r.clone()
 
 				# stopping condition
-				if torch.abs(r_m1 - r_m2) > r_step_min:
+				if torch.abs(r_m1 - r_m2) < r_step_min:
 					break
 				
 
@@ -145,13 +153,19 @@ def cc(X):
 			# test for convergence
 			Z_new = f_layer(Z)
 			if (Z_new - Z).pow(2).mean().sqrt() < 1e-4:
-				converge_counter += 1
-				if converge_counter >= n_stop_to_converge:
-					break
+				# if we don't make any progress, don't add layer. However, we only
+				# count convergence once radius is at its maximum
+				if r.item() == r_max:
+					converge_counter += 1
+					if converge_counter >= n_stop_to_converge:
+						break
+				else:
+					converge_counter = 0
 			else:
 				converge_counter = 0
 				f.add_operation(f_layer)
 				g.add_operation(g_layer)
+				d_prev = U.shape[1]
 				# only update representation if we add the layer
 				Z = Z_new.clone()
 
@@ -198,49 +212,59 @@ def opt_UV(Z, z_c, U_0, n_iter_inner, r=-1, kernel=-1):
 		z_mu = (Z*kernel_pre).sum(dim=0, keepdim=True) / kernel_pre.sum()
 		kernel = torch.exp(-gamma*(Z - z_mu).pow(2).sum(dim=1, keepdim=True))
 	
+	# find sub-selection for nonzero kernel elements
+	n_idx = kernel.flatten() > 1e-6
+	with torch.no_grad():
+		Z_train = Z[n_idx,:].detach().clone()
+		kernel_train = kernel[n_idx].detach().clone()
+	
 
-	for i in range(n_iter_inner):
+	for _ in range(n_iter_inner):
 		U_old = U.data.clone()
 		opt_U.zero_grad()
 		# opt_V.zero_grad()
 		# opt_alpha.zero_grad()
 
-		coord = (Z - z_c) @ U
-		Z_perp = (Z - z_c) - coord @ U.T
+		coord = (Z_train - z_c) @ U
+		Z_perp = (Z_train - z_c) - coord @ U.T
 
 		if not use_cross_terms:
 			
 			coord2 = coord.pow(2)
-			A = coord2 * kernel
-			b = Z_perp * kernel
+			A = coord2 * kernel_train
+			b = Z_perp * kernel_train
 
 			# least squares solution for V, note automatically orthogonal to U
 			with torch.no_grad():
-				V = ((A.T@A).inverse() @ (A.T@b)).T
+				# V = (torch.linalg.pinv(A, rtol=1e-6)@b).T
+				V = torch.linalg.lstsq(A, b, rcond=1e-4).solution.T
 
-			loss = 0.5*(kernel*(Z_perp - coord2@V.T)).pow(2).mean()
+			loss = 0.5*(kernel_train*(Z_perp - coord2@V.T)).pow(2).mean()
 
 		else:
 			# input coordinates for the exponential map hessian (second
 			# fundamental form)
 
+			# multiply batch of matrices
 			# output is a tensor of shape (N,d,d)
-			H_input = torch.bmm(coord.reshape((N,d,1)), coord.reshape((N,1,d)))
+			# H_input = torch.bmm(coord.reshape((N,d,1)), coord.reshape((N,1,d)))
+			H_input = torch.bmm(coord.unsqueeze(2), coord.unsqueeze(1))
 			# since tensor is symmetric, we only need to keep upper diag terms
 			# output is a tensor of shape (N, d(d+1)/2)
 			idx_triu = torch.triu_indices(d,d)
 			H_input = H_input[:,idx_triu[0,:],idx_triu[1,:]]
 
 			# construct A, b matrices for least squares
-			A = H_input * kernel
-			b = Z_perp * kernel
-
+			A = H_input * kernel_train
+			b = Z_perp * kernel_train
+			
 			# least squares solution for V, note automatically orthogonal to U
 			# output is of shape (D, d(d+1)/2)
 			with torch.no_grad():
-				V = ((A.T@A).inverse() @ (A.T@b)).T
+				# V = ((A.T@A).inverse() @ (A.T@b)).T
+				V = torch.linalg.lstsq(A, b, rcond=1e-4).solution.T
 
-			loss = 0.5*(kernel*(Z_perp - H_input@V.T)).pow(2).mean()
+			loss = 0.5*(kernel_train*(Z_perp - H_input@V.T)).pow(2).mean()
 
 		# loss = (U).pow(2).mean()
 		loss.backward()
@@ -265,20 +289,23 @@ def opt_UV(Z, z_c, U_0, n_iter_inner, r=-1, kernel=-1):
 	# 	print('Warning: U did not converge')		
 	return U.detach().data, V.detach().data, loss.detach()
 
-def find_d(Z, z_c, r_dimcheck, n_iter_inner):
+def find_d(Z, z_c, r_dimcheck, n_iter_inner, d_prev):
 	# We find the minimial d by iteratively fitting a model
 	# for some size d, then increase d and repeat if the max
 	# reconstruction error is too large
 
-	max_error_ratio = 0.4
+	max_error = 0.2 *  r_dimcheck
 
 	N, D = Z.shape
 	# init
-	U_0 = torch.randn(D, 1)
-	U_0 = U_0 / torch.norm(U_0, p=2)
+	U_0 = torch.randn(D, d_prev)
+	U_0 = torch.linalg.qr(U_0)[0]
+
+	# TRACKING VARS
+	was_decreasing = False
 
 	# init tracking variable
-	max_error = max_error_ratio*r_dimcheck
+	# max_error = max_error_ratio*r_dimcheck
 
 	# note kernel will stay the same for all d
 	gamma = float(np.log(2))/(r_dimcheck**2)
@@ -286,19 +313,43 @@ def find_d(Z, z_c, r_dimcheck, n_iter_inner):
 	z_mu = (Z*kernel_pre).sum(dim=0, keepdim=True) / kernel_pre.sum()
 	kernel = torch.exp(-gamma*(Z - z_mu).pow(2).sum(dim=1, keepdim=True))
 
-	while max_error >= max_error_ratio*r_dimcheck:
+	# need to track U_prev in case of decreasing
+	U_prev = U_0.clone()
+
+	# note this loop iterates a maximum of D times
+	for i in range(D):
 		U, V, loss = opt_UV(Z, z_c, U_0, n_iter_inner, kernel=kernel)
 
 		# compute max error, if too large, increase d
-		# coord = (Z - z_c) @ U
-		# Z_perp = (Z - z_c) - coord @ U.T
-		# coord2 = coord.pow(2)
-		# max_error = (kernel*(Z_perp - coord2@V.T)).norm(dim=1).max()
+		coord = (Z - z_c) @ U
+		Z_perp = (Z - z_c) - coord @ U.T
+		# NOTE THIS NEEDS TO CHANGE IF DOING DIAG HESSIAN
+		H_input = torch.bmm(coord.unsqueeze(2), coord.unsqueeze(1))
+		# since tensor is symmetric, we only need to keep upper diag terms
+		# output is a tensor of shape (N, d(d+1)/2)
+		d = U.shape[1]
+		idx_triu = torch.triu_indices(d,d)
+		H_input = H_input[:,idx_triu[0,:],idx_triu[1,:]]
+		l0_error = (kernel*(Z_perp - H_input@V.T)).norm(dim=1).max()
+		# if achieved desired loss, reduce dimension
+		# print(f'd: {d}, error: {l0_error}')
+		if l0_error <= max_error:
+			# if we were increasing but then achieved desired loss we have converged
+			if (not was_decreasing and i > 0) or U.shape[1] == 1:
+				break
 
-		# just use the computed error
-		max_error = loss
-		if max_error >= max_error_ratio*r_dimcheck:
-			# add new vec , orthogonalize via gram schmidt
+			# otherwise, continue to reduce dimension
+			was_decreasing = True
+			# remove column of U lease correlated with V
+			idx_min = ((Z - z_c) @ U).abs().mean(dim=0).argmin()
+			U_0 = torch.cat((U[:,:idx_min], U[:,idx_min+1:]), dim=1)
+			U_prev = U.clone()
+
+		else:
+			# if we were decreasing but then achieved desired loss we have converged
+			if was_decreasing or U.shape[1] == D:
+				U = U_prev
+				break
 			U_new = torch.randn(D, 1)
 			U_new = U_new - U @ (U.T@U_new)
 			U_new = U_new / torch.norm(U_new, p=2)
