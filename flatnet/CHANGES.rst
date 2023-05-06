179c78945a57ebfb0d05f078c84c9e9125c44036
Michael Psenka <michael.psenka@gmail.com>
2023-05-06 00:26:15 -0400

    added changelog

---
+++ flatnet/CHANGES.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+0.1.3 (2023-05-06)
+------------------
+
+Bug Fixes:
+- Added normalization of the input data
+
+0.1.1 (2023-05-06)
+------------------
+
+Features:
+- Added normalization of the input data
+
+0.1.0 (2022-12-31)
+------------------
+
+Initial release of flatnet!
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flatnet',
-    version='0.1.0',
+    version='0.1.2',
     description='FlatNet implementation in PyTorch, from the paper \"Representation Learning via Manifold Flattening and Reconstruction\"',
     author='Michael Psenka',
     packages=find_packages(),
